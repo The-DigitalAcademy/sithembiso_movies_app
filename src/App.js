833ff2d741a651817992efc227cbb3ee7bebf8be
sthezahadebe-jpg <ssithembisosifiso@gmail.com>
2022-07-20 16:30:45 +0200

    added header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,82 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import "./App.scss";
 
-import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h2>App</h2>
+    <div className='app'>
+      <Router>
+        <Header></Header>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/movie/:imdbID' component={MovieDetail} />
+          <Route component={PageNotFound} />
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+// import Home from "./components/Home/Home";
+// import Header from "./components/Header/Header";
+// import Footer from "./components/Footer/Footer";
+// import PageNotFound from './components/PageNotFound/PageNotFound';
+// import MovieDetail from "./components/MovieDetail/MovieDetail";
+// import "./App.scss";
+
+
+// const App = () => {
+//   return (
+//     <div className='App'>
+//       <Router>
+//         <Header></Header>
+//         <Route path='/' component={Home} />
+//         <Route path='/movie/:imdbID' component={MovieDetail} />
+//         <Route component={PageNotFound} />
+//         <Footer />
+//       </Router>
+//     </div>
+//   );
+// };
+
+// export default App;
