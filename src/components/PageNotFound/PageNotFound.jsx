040f9d9a363563755236e4e9da53a790018f60c5
sthezahadebe-jpg <ssithembisosifiso@gmail.com>
2022-08-01 09:31:47 +0200

    change color added

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Switch } from '@mui/material';
+import { Router } from 'react-router-dom';
+
 
 function PageNotFound() {
     return (
-        <img src="PageNotFound.jpeg" alt="" />
+        <Router>
+            <Switch>
+                <div className='pageNotFound'>
+                    <img src={PageNotFound.jpeg} height="500" width="" alt="not found" />  
+                </div>
+            </Switch>
+        </Router>
+        
     )
 }
 
-export default PageNotFound
+export default PageNotFound;
